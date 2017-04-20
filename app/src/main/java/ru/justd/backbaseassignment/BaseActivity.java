64aeaea6a14d3a2d0dc335405020dd01c3f2f1d0
defuera <vdenis@zvooq.com>
2017-04-20 11:51:59 +0200

    update gitignote

---
+++ app/src/main/java/ru/justd/backbaseassignment/BaseActivity.java
@@ -0,0 +1,10 @@
+package ru.justd.backbaseassignment;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by defuera on 20/04/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
